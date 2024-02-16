8f294a46ccb0d1491567ce5252b2889beade6cbc
kellykurowski <kellykurowski@live.nl>
2024-02-16 20:23:09 +0100

    Added Part 2 to Day4

--- 2022/Day4.py
+++ 2022/Day4.py
@@ -3,6 +3,7 @@ with open('input4.txt', 'r') as file:
 
 
 res = 0
+res2 = 0
 for line in content:
     pairs = line.split(",")
     first_pair_firstno = pairs[0].split("-")[0]
@@ -13,7 +14,12 @@ for line in content:
     first_pair = [x for x in range(int(first_pair_firstno), int(first_pair_secondno) + 1)]
     second_pair = [x for x in range(int(second_pair_firstno), int(second_pair_secondno) + 1)]
 
-    if all(elem in first_pair for elem in second_pair) or all(elem in second_pair for elem in first_pair):
-        res +=1
+    # Part 1
+    # if all(elem in first_pair for elem in second_pair) or all(elem in second_pair for elem in first_pair):
+    #     res +=1
+
+    if set(first_pair).intersection(set(second_pair)):
+        res2 += 1
 
 print("Part 1:", res)
+print("Part 2:", res2)
