00194f875228974893c061f0034b9daae62c5bef
kellykurowski <kellykurowski@live.nl>
2024-02-17 10:13:03 +0100

    Created stack list

--- 2022/Day5.py
+++ 2022/Day5.py
@@ -1,10 +1,22 @@
 with open('input5.txt', 'r') as file:
     stack, instructions = [part.split("\n") for part in file.read().split("\n\n")]
 
-# Find pile number for letter
+
+stacks = []
+
+# Append the letters to the pile
 for line in stack:
     new_line = "  " + line
-    for x in new_line:
-        if x.isalpha():
-            pile = (new_line.index(x) // 4) + 1
-            print(pile)
+    for i, x in enumerate(new_line):
+        if x.isalpha():  # Check if line contains letter
+            pile = (i // 4) + 1  # Calculate corresponding pile number
+            found = False
+            for item in stacks:
+                if item[0] == pile:  # Check if pile number already exists in stacks list
+                    item[1].append(x)
+                    found = True
+                    break
+            if not found:
+                stacks.append([pile, [x]])
+
+print(stacks)
