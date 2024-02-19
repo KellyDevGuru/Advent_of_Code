7cb4635bfa915a111064b18519a2cfe5832e9942
kellykurowski <k.kurowski@students.uu.nl>
2024-02-19 08:45:45 +0100

    Day 6 Part 2 Solution

--- 2022/Day6.py
+++ 2022/Day6.py
@@ -10,3 +10,13 @@ for index, x in enumerate(line[4:]):
         chars.append(x)
         del chars[0]
 
+chars2 = [x for x in line[0:14]]
+for index, x in enumerate(line[14:]):
+    if len(set(chars2)) == 14:
+        print("Part 2:", index + 14)  # Add 14 to index because enumerate starts at index 14
+        break
+    else:
+        chars2.append(x)
+        del chars2[0]
+
+
