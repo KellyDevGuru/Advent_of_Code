58383f86c102dff6e4d20616b394c7046c718887
kellykurowski <k.kurowski@students.uu.nl>
2024-02-26 22:11:45 +0100

    Day 9 Added distance calculation

--- 2022/Day9.py
+++ 2022/Day9.py
@@ -1,3 +1,5 @@
+import math
+
 with open('input9.txt', 'r') as file:
     lines = [x.strip() for x in file.readlines()]
 
@@ -6,30 +8,35 @@ coordinates = []
 
 for c in lines:
 
-    if c[0] == 'D':
-        for i in range(int(c[2])):
-            coordinates.append((x, (y + i + 1)))
-        y += int(c[2])
-
     if c[0] == 'U':
         for i in range(int(c[2])):
-            coordinates.append((x, (y - i - 1)))
+            coordinates.append(((x, (y + i + 1)), c[0]))
+        y += int(c[2])
+
+    if c[0] == 'D':
+        for i in range(int(c[2])):
+            coordinates.append(((x, (y - i - 1)), c[0]))
         y -= int(c[2])
 
     if c[0] == 'L':
         for i in range(int(c[2])):
-            coordinates.append(((x - i - 1), y))
+            coordinates.append((((x - i - 1), y), c[0]))
         x -= int(c[2])
 
     if c[0] == 'R':
         for i in range(int(c[2])):
-            coordinates.append(((x + i + 1), y))
+            coordinates.append((((x + i + 1), y), c[0]))
         x += int(c[2])
 
-    # Update coordinate after each move
-    coordinate = (x, y)
-    print(coordinate)
+
+def calculate_distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
 
 
-for x in range(2):
-    print(x)
+
+
+
+
