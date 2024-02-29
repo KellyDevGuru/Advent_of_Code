b1c7cfbb596a7efa98d1b1642f74f24f6fba2954
kellykurowski <k.kurowski@students.uu.nl>
2024-02-29 09:49:39 +0100

    Day 9 new logic

--- 2022/Day9.py
+++ 2022/Day9.py
@@ -4,39 +4,56 @@ with open('input9.txt', 'r') as file:
     lines = [x.strip() for x in file.readlines()]
 
 x = y = 0
+xt = yt = 0
 coordinates = []
+coordinatest = []
 
 for c in lines:
 
     if c[0] == 'U':
         for i in range(int(c[2])):
-            coordinates.append(((x, (y + i + 1)), c[0]))
+            coordinates.append((x, (y + i + 1)))
+            if x == xt:
+                coordinatest.append((xt, (yt + i)))
+            else:
+                xt = x
+                coordinatest.append((xt, (yt + i)))
         y += int(c[2])
+        yt += (int(c[2]) - 1)
 
     if c[0] == 'D':
         for i in range(int(c[2])):
-            coordinates.append(((x, (y - i - 1)), c[0]))
+            coordinates.append((x, (y - i - 1)))
+            if x == xt:
+                coordinatest.append((xt, (yt - i)))
+            else:
+                xt = x
+                coordinatest.append((xt, (yt - i)))
+
         y -= int(c[2])
+        yt -= (int(c[2]) - 1)
 
     if c[0] == 'L':
         for i in range(int(c[2])):
-            coordinates.append((((x - i - 1), y), c[0]))
+            coordinates.append(((x - i - 1), y))
+            if y == yt:
+                coordinatest.append(((xt - i), yt))
+            else:
+                yt = y
+                coordinatest.append(((xt - i), yt))
+
         x -= int(c[2])
+        xt -= (int(c[2]) - 1)
 
     if c[0] == 'R':
         for i in range(int(c[2])):
-            coordinates.append((((x + i + 1), y), c[0]))
+            coordinates.append(((x + i + 1), y))
+            if y == yt:
+                coordinatest.append(((xt + i), yt))
+            else:
+                yt = y
+                coordinatest.append(((xt + i), yt))
         x += int(c[2])
+        xt += (int(c[2]) - 1)
 
-
-def calculate_distance(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    return distance
-
-
-
-
-
-
+print(len(set(coordinatest)))
