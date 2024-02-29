89aa2eac2f3def1210515fdd3c8fa6dc2edf7f89
kellykurowski <k.kurowski@students.uu.nl>
2024-02-29 15:58:42 +0100

    Day 9 Part 1 Solution

--- 2022/Day9.py
+++ 2022/Day9.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
+import math
 
-with open('inputtest.txt', 'r') as file:
+with open('input9.txt', 'r') as file:
     lines = [x.strip() for x in file.readlines()]
 
 x = y = 0
@@ -8,62 +9,66 @@ xt = yt = 0
 coordinates = []
 coordinatest = []
 
+
+def calculate_distance(x1, y1, x2, y2):
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+
 for c in lines:
+    n = c.split()
 
-    if c[0] == 'U':
-        for i in range(int(c[2])):
-            coordinates.append((x, (y + i + 1)))
-            if x == xt:
-                coordinatest.append((xt, (yt + i)))
+    if n[0] == 'U':
+        for i in range(int(n[1])):
+            y += 1
+            coordinates.append((x, y))
+            if calculate_distance(x,y,xt,yt) <= math.sqrt(2):
+                coordinatest.append((xt,yt))
             else:
                 xt = x
-                coordinatest.append((xt, (yt + i)))
-        y += int(c[2])
-        yt += (int(c[2]) - 1)
+                yt = y - 1
+                coordinatest.append((xt,yt))
 
-    if c[0] == 'D':
-        for i in range(int(c[2])):
-            coordinates.append((x, (y - i - 1)))
-            if x == xt:
-                coordinatest.append((xt, (yt - i)))
+    if n[0] == 'D':
+        for i in range(int(n[1])):
+            y -= 1
+            coordinates.append((x, y))
+            if calculate_distance(x, y, xt, yt) <= math.sqrt(2):
+                coordinatest.append((xt, yt))
             else:
                 xt = x
-                coordinatest.append((xt, (yt - i)))
+                yt = y + 1
+                coordinatest.append((xt, yt))
 
-        y -= int(c[2])
-        yt -= (int(c[2]) - 1)
-
-    if c[0] == 'L':
-        for i in range(int(c[2])):
-            coordinates.append(((x - i - 1), y))
-            if y == yt:
-                coordinatest.append(((xt - i), yt))
+    if n[0] == 'L':
+        for i in range(int(n[1])):
+            x -= 1
+            coordinates.append((x, y))
+            if calculate_distance(x, y, xt, yt) <= math.sqrt(2):
+                coordinatest.append((xt, yt))
             else:
                 yt = y
-                coordinatest.append(((xt - i), yt))
+                xt = x + 1
+                coordinatest.append((xt, yt))
 
-        x -= int(c[2])
-        xt -= (int(c[2]) - 1)
-
-    if c[0] == 'R':
-        for i in range(int(c[2])):
-            coordinates.append(((x + i + 1), y))
-            if y == yt:
-                coordinatest.append(((xt + i), yt))
+    if n[0] == 'R':
+        for i in range(int(n[1])):
+            x += 1
+            coordinates.append((x, y))
+            if calculate_distance(x, y, xt, yt) <= math.sqrt(2):
+                coordinatest.append((xt, yt))
             else:
                 yt = y
-                coordinatest.append(((xt + i), yt))
-        x += int(c[2])
-        xt += (int(c[2]) - 1)
+                xt = x - 1
+                coordinatest.append((xt, yt))
 
 print(len(set(coordinatest)))
-
 
 def draw_coordinates(c):
     x_values = [coord[0] for coord in c]
     y_values = [coord[1] for coord in c]
 
-    plt.plot(x_values, y_values, color = 'red', markersize=2)
+    plt.plot(x_values, y_values, 'ro', markersize=1)
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Coordinates')
@@ -72,4 +77,5 @@ def draw_coordinates(c):
 
 
 draw_coordinates(coordinatest)
+
 
